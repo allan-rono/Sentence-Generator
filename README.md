10f9972832dab1172e9a57b24e0bed1df3217e9b
Allan Rono <allanrono@hotmail.com>
2021-03-29 17:30:47 +1100

    completed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sentence-Generator
 Author: Allan Rono
+
+# Sentence Structure
+There are three types of sentences: simple, compound, and complex. The type of sentence is determined by how many clauses, or subject–verb groups, are included in the sentence. A simple sentence structure has one independent clause: “I rode my bike.” A compound sentence has at least two independent clauses: “I got in my car, and I drove into town.” In that sentence, both clauses can stand on their own as complete sentences. A complex sentence includes an independent clause and one or more dependent clauses: “I got in my car and then went to town.” In that sentence, “I got in my car” works as a complete sentence but “then went to town” does not.
+
+# Source
+https://codepen.io/chiragbhansali/pen/EWppvy
+https://www.wordy.com/writers-workshop/basic-english-sentence-structure/
--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const display = document.getElementById("display");
 const generate = document.getElementById("generate");
 
-const noun = [];
-const verb = [];
-const object = [];
+const nouns = [ "bird", "clock", "boy", "plastic",
+                "duck", "teacher", "old lady", "professor", "hamster", "dog"];
+const verbs = [ "kicked", "ran", "flew", "dodged",
+                "sliced", "rolled", "died", "breathed", "slept", "killed"];
+const adverbs = ["slowly", "elegantly", "precisely",
+                 "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+const preposition = ["down", "into", "up", "on", "upon",
+                     "below", "above", "through", "across", "towards"];
+const adjectives = ["beautiful", "lazy", "professional",
+                     "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
 
 //Generate Simple sentence Subject(noun)-Verb-Object
-function generateSentence(){};
+function genRand(){
+    return Math.floor(Math.random()*5);
+}
+
+function generateSentence(){
+    var rand1 = Math.floor(Math.random() * 10);
+    var rand2 = Math.floor(Math.random() * 10);
+    var rand3 = Math.floor(Math.random() * 10);
+    var rand4 = Math.floor(Math.random() * 10);
+    var rand5 = Math.floor(Math.random() * 10);
+    var rand6 = Math.floor(Math.random() * 10);
+
+    var content = "The " + adjectives[rand1] +
+     " " + nouns[rand2] + " " + adverbs[rand3] +
+    " " + verbs[rand4] + " because some " +
+    nouns[rand1] + " " + adverbs[rand1] +
+    " " + verbs[rand1] + " " + preposition[rand1] +
+    " a " + adjectives[rand2] + " " + nouns[rand5] +
+    " which, became a " + adjectives[rand3] +
+    ", " + adjectives[rand4] + " " + nouns[rand6] + "."; 
+
+    display.innerHTML = "&quot;" + content + "&quot;";
+};
 
 //Event Listeners
 generate.addEventListener("click",generateSentence)
+

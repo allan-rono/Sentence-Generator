03c65da59c988f663b8fb316e0b4cd1c4e4ba1ac
Allan Rono <allanrono@hotmail.com>
2021-03-29 17:04:33 +1100

    js skeleton implemented

---
+++ script.js
@@ -0,0 +1,12 @@
+const display = document.getElementById("display");
+const generate = document.getElementById("generate");
+
+const noun = [];
+const verb = [];
+const object = [];
+
+//Generate Simple sentence Subject(noun)-Verb-Object
+function generateSentence(){};
+
+//Event Listeners
+generate.addEventListener("click",generateSentence)
